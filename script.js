13f401f419e65258ebabfae285d2e16d9a4c4315
Andre023 <andrelucas25111@gmail.com>
2023-08-29 17:47:29 -0300

    Alteração do maps e do rodapé

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-<script>
-  // Obtém todos os botões do menu
-  const menuItems = document.querySelectorAll(".cabecalho-menu-item");
+document.addEventListener("DOMContentLoaded", function () {
+  let mapInitialized = false;
 
-  // Função para adicionar a classe 'botao-clicado' ao botão clicado
-  function marcarBotaoClicado(event) {
-    // Remove a classe 'botao-clicado' de todos os botões
-    menuItems.forEach((item) => item.classList.remove("botao-clicado"));
-    // Adiciona a classe 'botao-clicado' ao botão clicado
-    event.target.classList.add("botao-clicado");
+  const mapLink = document.getElementById("lauras-mapLink");
+  const mapModal = document.getElementById("lauras-mapModal");
+  const closeModal = document.getElementById("lauras-closeModal");
+
+  function initMap() {
+    const location = { lat: -20.385574, lng: -43.503578 };
+    const map = new google.maps.Map(document.getElementById("lauras-modalMap"), {
+      zoom: 15,
+      center: location
+    });
+    new google.maps.Marker({ position: location, map: map });
+    google.maps.event.trigger(map, 'resize');  // Redefine o tamanho do mapa
+    map.setCenter(location); // Re-centraliza o mapa
   }
 
-  // Adiciona o evento de clique a cada botão do menu
-  menuItems.forEach((item) => item.addEventListener("click", marcarBotaoClicado));
-</script>
+  mapLink.addEventListener("click", function (event) {
+    event.stopPropagation();
+    const rect = mapLink.getBoundingClientRect();
+    mapModal.style.left = (rect.left + window.pageXOffset + rect.width / 2 - 100) + "px"; 
+    mapModal.style.top = (rect.top + window.pageYOffset - 2260) + "px";  // Ajustado para -150
+    mapModal.style.display = "block";
+    initMap();
+  });
+  
+
+  closeModal.addEventListener("click", function (event) {
+    event.stopPropagation();
+    mapModal.style.display = "none";
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!mapModal.contains(event.target) && event.target !== mapLink) {
+      mapModal.style.display = "none";
+    }
+  });
+  
+});
+
+const menu=document.querySelector(".menu");
+const toggle=document.querySelector(".toggle");
+toggle.addEventListener("click",()=>{
+  menu.classList.toggle("active");
+})
